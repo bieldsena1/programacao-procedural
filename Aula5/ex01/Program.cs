179dca25603c5d6c97484cc4060ccb396e3908e2
bieldsena1 <gabrieldsenaeduardo@gmail.com>
2024-03-02 11:26:01 -0300

    Aulas

---
+++ Aula5/ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if(numero %  2 == 0)
+        {
+            Console.Write("NUMERO EH PAR");
+        }
+        else
+        {
+            Console.Write("NUMERO EH IMPAR");
+        }
+    }
+}
+
+
