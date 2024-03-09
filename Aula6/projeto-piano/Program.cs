f36a67352d53571972969357dd739d8491c781ce
bieldsena1 <gabrieldsenaeduardo@gmail.com>
2024-03-09 11:21:54 -0300

    atualizando

---
+++ Aula6/projeto-piano/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int notas = 1;
+       1
+        while(notas <= 7)
+        {
+            Piano.ReproduzTecla(1,notas);
+            notas++;
+        }
+
+    }
+}
---
+++ Aula8/ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+        for(int linha = 0; linha < 8; linha++)
+        {
+            for(int coluna = 0; coluna < 8; coluna++)
+            {
+                Tabuleiro.Adicionar(linha, coluna, "biel");
+            }
+        }
+        Tabuleiro.ImprimeTabuleiro();
+
+    }
+}
