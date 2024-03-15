79b2ae4dae82324a637aa7a8bd48158001a86841
Gabriel de Sena <gabrieldsenaeduardo@gmail.com>
2024-03-15 15:14:45 -0300

    LISTA 4

---
+++ LISTA_4/ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        const int TABUADA = 10;
+        for(int numero1 = 0; numero1 <= TABUADA; numero1++)
+        {
+            Console.WriteLine("\nTabuada do " + numero1);
+            for(int numero2 = 0; numero2 <= TABUADA; numero2++)
+            {
+                Console.Write(numero1 + " X " + numero2 + " = "+ (numero1 * numero2) + "\n");
+            }
+
+
+        }
+    }
+}
