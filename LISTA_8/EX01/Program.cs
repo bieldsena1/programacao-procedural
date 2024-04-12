396124f6087f99537a47c890bcecd858c970b083
Gabriel de Sena <gabrieldsenaeduardo@gmail.com>
2024-04-12 07:51:54 -0300

    lista 8

---
+++ LISTA_8/EX01/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+using System.Runtime.InteropServices;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza;
+
+        pizza.id = int.Parse(Console.ReadLine());
+        pizza.sabor = Console.ReadLine();
+        pizza.preco = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(pizza.id);
+        System.Console.WriteLine(pizza.sabor);
+        System.Console.WriteLine(pizza.preco);
+    }
+}
---
+++ LISTA_8/EX02/Program.cs
@@ -0,0 +1,35 @@
+﻿﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int VETOR = 4;
+        Produto[] produtos = new Produto[VETOR];
+
+        for (int i = 0; i < 4; i++)
+        {
+            produtos[i].id = int.Parse(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = double.Parse(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            if(produtos[i].disponivelEmEstoque)
+            {
+                System.Console.WriteLine(produtos[i].nome);
+                System.Console.WriteLine(produtos[i].preco);
+            }
+        }
+    }
+}
